bb1eae0845b82144640f43bd8c94d2d2b8bde99e
Jan Melcher <mail@jan-melcher.de>
2015-05-16 23:38:38 +0200

    Extract model logic

---
+++ scripts/Model.js
@@ -0,0 +1,50 @@
+define(['Node', 'EventEmitter'], function(Node, EventEmitter) {
+	var algorithms = ['SD.Use', 'SD.Agg', 'CC.I'];
+	var project = location.search ? location.search.substring(1) : 'PMD';
+	var trees = [];
+
+	function load() {
+		var treeNames = algorithms.slice(0);
+		treeNames.push('packages');
+		treeNames.forEach(function(name) {
+			fetchTree(name, function(tree) {
+				trees.push(tree);
+				if (trees.length == treeNames.length) {
+					onReady();
+				}
+			});
+		})
+	}
+
+	function fetchTree(name, success) {
+		d3.json("data/" + project + "/" + name + ".json", function (error, tree) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			tree.root = new Node(tree.root);
+			tree.root.normalizeOnlyChilds();
+			success(tree);
+		});
+	}
+
+	function onReady() {
+		Model.emit('ready');
+	}
+
+	var Model = new EventEmitter();
+
+	Model.getTrees = function() { return trees; };
+
+	Model.getTree = function(name) {
+		var sel = trees.filter(function(t) { return t.couplingConcept == name});
+		if (sel.length) {
+			return sel[0];
+		}
+		throw new Error('Tree ' + name + ' does not exist');
+	};
+
+	load();
+
+	return Model;
+});
--- scripts/main.js
+++ scripts/main.js
@@ -3,33 +3,21 @@ require.config({
 		"EventEmitter": "../bower_components/eventEmitter/EventEmitter"
 	}
 });
-define(['Icicle', 'Node'], function(Icicle, Node) {
-	var project = location.search ? location.search.substring(1) : 'PMD';
 
-	fetchTree('packages', function (packages) {
-		packages.root.normalizeOnlyChilds();
-		Icicle.create(packages, '#icicles');
-
-		var others = ['SD.Use', 'SD.Agg', 'CC.I'];
-		others.forEach(function (otherName) {
-			fetchTree(otherName, function (other) {
-				other.root.normalizeOnlyChilds();
-				Icicle.create(other, '#icicles', packages);
-			});
+define(['Icicle', 'Model'], function(Icicle, Model) {
+	Model.on('ready', function() {
+		var packagesTree = Model.getTree('packages');
+		Icicle.create(packagesTree, '#icicles');
+		Model.getTrees().forEach(function(tree) {
+			if (tree.couplingConcept == 'packages') {
+				return;
+			}
+			Icicle.create(tree, '#icicles', packagesTree);
 		});
 	});
 
-	function fetchTree(coupling, success) {
-		d3.json("data/" + project + "/" + coupling + ".json", function (error, tree) {
-			if (error) {
-				console.log(error);
-				return;
-			}
-			tree.root = new Node(tree.root);
-			success(tree);
-		});
-	}
-
+	// project selection
+	var project = location.search ? location.search.substring(1) : 'PMD';
 	$('#project').change(function () {
 		project = $(this).val();
 		location.search = '?' + project;
