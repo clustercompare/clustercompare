e8bbc78cb156d19387f896f930a34774306fe686
Jan Melcher <mail@jan-melcher.de>
2015-06-12 02:11:19 +0200

    explicitly specify syntax highlighting language

--- src/SourceBrowser.js
+++ src/SourceBrowser.js
@@ -4,7 +4,7 @@ import hljs from 'highlight.js'
 export function showForClass(clazz) {
 	var url = 'data/' + clazz.getRoot().project + '/src/' + clazz.data.qualifiedName.replace(/\./g, '/') + '.java';
 	$.get(url, function(contents) {
-		$('#source-browser .source').text(contents);
+		$('#source-browser .source').text(contents).addClass('java');
 		hljs.highlightBlock($('#source-browser .source')[0]);
 		$('#icicles').hide();
 		$('#source-browser').show();
