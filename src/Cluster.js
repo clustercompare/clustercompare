a37ddedcaf25a4afb181c51a682a1e527389bb47
Jan Melcher <mail@jan-melcher.de>
2015-06-25 16:33:33 +0200

    Show cluster numbers in title text

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,4 +1,14 @@
 import InnerNode from './InnerNode';
 
 export default class Cluster extends InnerNode {
+	getLabel() {
+		return "Cluster " + this.getCompleteKey();
+	}
+
+	getCompleteKey() {
+		if (this.getParent() instanceof Cluster) {
+			return this.getParent().data.key + '.' + this.data.key;
+		}
+		return this.data.key;
+	}
 }
