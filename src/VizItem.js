95063446c41e7c1fb0731ec68231e2d32186280c
Jan Melcher <info@yogularm.de>
2015-06-28 15:51:34 +0200

    Make viz items draggable

--- src/VizItem.js
+++ src/VizItem.js
@@ -6,11 +6,64 @@ import $ from 'jquery';
  * The frame around an icicle that can be rearranged
  */
 export default class VizItem extends EventEmitter {
+	_dragging = false;
+	_placeholder = null;
+	_dragCursorStartX;
+	_dragtElementStartX;
+
 	constructor(tree, containerSelector, viewModel) {
 		super();
 		this._element =  $('<div>').addClass('viz-item').appendTo($(containerSelector));
-		this._element.append($('<h3>').text(tree.couplingConcept));
+		this._heading = $('<h3>').text(tree.couplingConcept);
+		this._element.append(this._heading);
 		var icicleContainer = $('<div>').addClass('icicle').appendTo(this._element);
 		this._icicle = new BoundIcicle(tree, icicleContainer[0], viewModel);
+
+		this._heading.on('mousedown', e => this._startDrag(e));
+	}
+
+	_startDrag(e) {
+		if (this._dragging) {
+			return;
+		}
+		e.preventDefault();
+		this._dragging = true;
+		var offset = this._element.offset();
+		this._placeholder = $('<div>').addClass('viz-item placeholder')
+				.width(this._element.width())
+				.height(this._element.height())
+				.insertBefore(this._element);
+		this._element.addClass('dragging');
+		this._element.css({left: offset.left, top: offset.top});
+		this._dragCursorStartX = e.pageX;
+		this._dragElementStartX = offset.left;
+		this._endDragFn = e => this._endDrag(e);
+		this._dragFn = e => this._drag(e);
+		$(document).on('mouseup', this._endDragFn);
+		$(document).on('mousemove', this._dragFn);
+	}
+
+	_drag(e) {
+		if (!this._dragging) {
+			return;
+		}
+		e.preventDefault();
+
+		var diff = e.pageX - this._dragCursorStartX;
+		this._element.css({left: this._dragElementStartX + diff});
+		this.emit('drag', { centerX: this._dragElementStartX + diff + this._element.width()});
+	}
+
+	_endDrag(e) {
+		if (!this._dragging) {
+			return;
+		}
+		this._placeholder.remove();
+		this._element.removeClass('dragging');
+		this._dragging = false;
+		$(document).off('mouseup', this._endDragFn);
+		$(document).off('mousemove', this._dragFn);
+		this._endDragFn = null;
+		this._dragFn = null;
 	}
 }
