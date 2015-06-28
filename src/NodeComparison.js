df7adcd2d61a54bf509c465b4e055d41e62d36b3
Jan Melcher <info@yogularm.de>
2015-06-28 18:56:53 +0200

    Exclude node roots from 'most similar clusters'

--- src/NodeComparison.js
+++ src/NodeComparison.js
@@ -12,20 +12,28 @@ export function getSimilarityOfLeaves(leaveSet1, leaveSet2) {
 }
 
 export function getMaxSimilarityInfoOfLeaveSetToNode(leaveSet, node) {
+	var info = { similarity: 0, node: null, totalCount: 0, intersection: 0 };
 	var intersection = Sets.intersect(leaveSet, node.leaveKeys).size;
-
 	if (!intersection) {
 		// no way any node of this subtree could be similar to the other node
-		return { similarity: 0, node: null, totalCount: 0, intersection: 0 };
+		return info;
 	}
 
-	var totalCount = Sets.merge(leaveSet, node.leaveKeys).size;
-	var similarity = intersection / totalCount;
+	// similarity of root nodes is never interesting
+	if (!node.isRoot) {
+		var totalCount = Sets.merge(leaveSet, node.leaveKeys).size;
+		var similarity = intersection / totalCount;
 
-	var info = { similarity: similarity, node: node, totalCount: totalCount, intersection: intersection };
-	if (similarity == 1) {
-		// already max
-		return info;
+		info = {
+			similarity: similarity,
+			node: node,
+			totalCount: totalCount,
+			intersection: intersection
+		};
+		if (similarity == 1) {
+			// already max
+			return info;
+		}
 	}
 
 	for (var child of node.children) {
