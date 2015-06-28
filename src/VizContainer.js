ad027047e23623b95440adba0601713adf2469ae
Jan Melcher <info@yogularm.de>
2015-06-28 19:17:31 +0200

    Fix dragging viz items to the end

--- src/VizContainer.js
+++ src/VizContainer.js
@@ -7,28 +7,31 @@ export default class VizContainer {
 	_items;
 
 	constructor(viewModel, containerSelector) {
-		this._element = $('<div>').addClass('viz-container');
-		this._items = model.trees.map(tree => new VizItem(tree, '#icicles', viewModel));
+		this._element = $('<div>').addClass('viz-container').appendTo($(containerSelector));
+		this._items = model.trees.map(tree => new VizItem(tree, this._element[0], viewModel));
 		$(containerSelector).click(function (e) {
 			if (e.ctrlKey) {
 				return;
 			}
 			viewModel.mainSelection.select(null);
 		});
-		for (var vizItem of this._items) {
+		for (let vizItem of this._items) {
 			vizItem.on("drag", e => {
-				var hoveredItem = this._findItemAtPos(e.centerX);
+				var hoveredItem = this._findItemAtPos(e.centerX, vizItem);
 				if (hoveredItem) {
 					e.moveBefore(hoveredItem.element);
 				} else {
-					e.moveAfter(this._items[this._items.length - 1].element);
+					e.moveAfter(this._element.children().filter(":not(.dragging):not(.placeholder)").last()[0]);
 				}
 			});
 		}
 	}
 
-	_findItemAtPos(x) {
-		for (var item of this._items) {
+	_findItemAtPos(x, excluding) {
+		for (let item of this._items) {
+			if (item == excluding) {
+				continue;
+			}
 			if ($(item.element).offset().left + $(item.element).width() / 2 >= x) {
 				return item;
 			}
