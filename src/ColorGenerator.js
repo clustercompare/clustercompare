ebb8a9fee7867161464eb6d0d67ec3bb9245dbb1
Jan Melcher <mail@jan-melcher.de>
2015-07-05 15:20:12 +0200

    fix color generation for packages clustering

--- src/ColorGenerator.js
+++ src/ColorGenerator.js
@@ -4,7 +4,7 @@ import sha256 from 'sha256';
 export function colorForClustering(clusteringName) {
 	let [group, instance] = clusteringName.split('.', 2);
 	let hue = stringHash(group) * 360;
-	let lightness = stringHash(instance);
+	let lightness = instance ? stringHash(instance) : 0.5;
 	return d3.hsl(hue, 0.5, lightness * 0.5 + 0.25);
 }
 
