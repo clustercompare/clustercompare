0da5c17827c94fe21d16db4704159c95c48e42ac
Jan Melcher <mail@jan-melcher.de>
2015-09-13 22:34:53 +0200

    Use more preformant moveInArray() implementation

--- src/ArrayUtils.js
+++ src/ArrayUtils.js
@@ -1,3 +1,10 @@
+// source: https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
 export function moveInArray(array, from, to) {
+    if (to >= array.length) {
+        var k = to - array.length;
+        while ((k--) + 1) {
+            array.push(undefined);
+        }
+    }
     array.splice(to, 0, array.splice(from, 1)[0]);
 }
