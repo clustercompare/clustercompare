ef8a462cf2fdef12555868852b5a3707d71115b3
Jan Melcher <mail@jan-melcher.de>
2015-09-25 16:02:31 +0200

    Provide buttons to select all, none and invert selection of clusterings

--- src/ClusteringSelector.js
+++ src/ClusteringSelector.js
@@ -11,8 +11,13 @@ export function init(viewModel_) {
     pane.html(template(viewModel.model.clusterings)).addClass('with-footer');
     pane.find('.apply-button').click(apply);
     pane.find('.reset-button').click(reset);
+    pane.find('.select-all-button').click(selectAll);
+    pane.find('.select-none-button').click(selectNone);
+    pane.find('.invert-selection-button').click(invertSelection);
+    pane.find('.clustering-checkbox').click(enableApplyAndResetButtons);
     hide();
     reset();
+    disableApplyAndResetButtons();
 }
 
 export function show() {
@@ -40,10 +45,35 @@ function apply() {
     let checkboxes = Array.from(pane.find('.clustering-checkbox:checked'));
     let keys = checkboxes.map(b => $(b).data('clustering'));
     viewModel.selectedClusterings.selectItemsPreserveOrder(keys);
+    disableApplyAndResetButtons();
 }
 
 function reset() {
     pane.find('.clustering-checkbox').each(function() {
         $(this).prop('checked', viewModel.selectedClusterings.items.indexOf($(this).data('clustering')) >= 0);
     });
+    disableApplyAndResetButtons();
+}
+
+function selectAll() {
+    pane.find('.clustering-checkbox').prop('checked', true);
+    enableApplyAndResetButtons();
+}
+
+function selectNone() {
+    pane.find('.clustering-checkbox').prop('checked', false);
+    enableApplyAndResetButtons();
+}
+
+function invertSelection() {
+    pane.find('.clustering-checkbox').prop('checked', function() { return !$(this).prop('checked'); });
+    enableApplyAndResetButtons();
+}
+
+function disableApplyAndResetButtons() {
+    pane.find('.apply-button, .reset-button').prop('disabled', true);
+}
+
+function enableApplyAndResetButtons() {
+    pane.find('.apply-button, .reset-button').prop('disabled', false);
 }
