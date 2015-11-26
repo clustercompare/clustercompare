e3eb40d77963dd347972ec2595efb9b5a3f336f4
Jan Melcher <mail@jan-melcher.de>
2015-11-26 20:41:56 +0100

    Use isodominant colors for clusterings

--- src/ColorGenerator.js
+++ src/ColorGenerator.js
@@ -1,14 +1,16 @@
 import d3 from 'd3';
 import sha256 from 'sha256';
 
+const ISODOMINANT_COLORS = true;
+
 export function colorForClustering(clusteringName) {
 	if (clusteringName == 'packages') {
-		return d3.rgb('#808080');
+		return d3.hsl(0, 0, 0.5); // grey
 	}
 
 	let [group, instance] = clusteringName.split('.', 2);
 	let hue = stringHash(group) * 360;
-	let lightness = instance ? stringHash(instance) : 0.5;
+	let lightness = !ISODOMINANT_COLORS && instance ? stringHash(instance) : 0.5;
 	return d3.hsl(hue, 0.5, lightness * 0.5 + 0.25);
 }
 
