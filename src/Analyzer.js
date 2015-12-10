5d0f5080abe58464028c232e029f840824c867a5
Jan Melcher <mail@jan-melcher.de>
2015-12-10 14:42:46 +0100

    Extend Analyzer

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -6,11 +6,18 @@ export default class Analyzer {
 	}
 
 	countClusteringWins() {
-		let trees = model.packagesTree.root.innerNodes.map(node => ArrayUtils.maxByValue(
-			model.couplingTrees.map(tree => node.getMaxSimilarityInfo(tree.root)),
-			info => info.similarity)); 
-		return trees;
+		let infos = model.packagesTree.root.innerNodes.map(node => ArrayUtils.maxByValue(
+			model.coupling3rees.map(tree => node.getMaxSimilarityInfo(tree.root)),
+			info => info.similarity));
+		return ArrayUtils.mapValues(ArrayUtils.groupBy(infos, info => info.node.root.clustering), list => list.length);
+	}
 
-		return ArrayUtils.mapValues(ArrayUtils.groupBy(trees, tree => tree.root.clustering), list => list.length);
+	getClusteringWinsAsCSV() {
+			let map = this.countClusteringWins();
+			let result = '';
+			let keys = model.clusterings.instances.map(i => i.key);
+			let headerCSV = keys.join(',');
+			let valuesCSV = keys.map(key => map[key] || 0).join(',');
+			return headerCSV + "\n" + valuesCSV;
 	}
 }
