e214dd65b19bd53127b4b938dfee74896f97e96b
Jan Melcher <mail@jan-melcher.de>
2016-01-07 00:16:11 +0100

    Black and white pie charts

--- src/PieChartRenderer.js
+++ src/PieChartRenderer.js
@@ -1,15 +1,21 @@
 export default function renderPieChart(context, x, y, radius, value) {
-	context.fillStyle = 'green';
+	context.fillStyle = 'black';
+	context.beginPath();
+	drawArc(context, x, y, radius, 0, 1);
+	context.fill();
+
+	context.fillStyle = 'white';
+	context.beginPath();
 	drawArc(context, x, y, radius, 0, value.pieChartValuePositive / 2);
-	context.fillStyle = 'red';
+	context.fill();
+	context.beginPath();
 	drawArc(context, x, y, radius, -value.pieChartValueNegative / 2, 0);
+	context.fill();
 }
 
 function drawArc(context, x, y, radius, start, end) {
 	let transformAngle = a => (a - 0.5) * Math.PI * 2;
-	context.beginPath();
 	context.moveTo(x, y);
-	context.arc(x, y, radius, transformAngle(start), transformAngle(end));
+	context.arc(x, y, radius, transformAngle(start), transformAngle(end), false);
 	context.lineTo(x, y);
-	context.fill();
 }
